7a4bea7c99c78aa30e7f464c1cbd7adf2b095b81
Matheus Augusto <matheusamg@gmail.com>
2025-06-18 15:47:16 -0300

    Contador de bits com Leds
    Cada um segundo acende uma sequencia de leds representando uma contagem. Ex: 00000010 dois segundos se passaram e o segundo led fica aceso.

--- led_counter.v
+++ led_counter.v
@@ -6,5 +6,28 @@ module Counter #(
     output wire [7:0] leds
 );
 
+localparam  ONE_SECOND = CLK_FREQ;
+
+reg [31:0] counter;
+reg [7:0] counter_8_bits;
+
+assign leds = counter_8_bits;
+
+always @(posedge clk) begin
+    if(!rst_n)begin
+        counter <= 0;
+        counter_8_bits <= 0;
+    end
+    else begin
+        if (counter >= ONE_SECOND -1) begin
+            counter <= 0;
+            counter_8_bits <= counter_8_bits + 1;
+        end
+        else begin
+            counter <= counter + 1;
+        end
+    end
+    
+end
 
 endmodule
